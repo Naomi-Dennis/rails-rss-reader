1b6c132ed167ace5c13c44028d78207387d44e0d
Naomi-Dennis <njdennis94@gmail.com>
2018-08-06 15:02:14 -0400

    Remove View Feed and Edit Feed

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,9 @@ Rails.application.routes.draw do
   post "/login", to: "users#process_login", as: "process_login"
   get "/logout", to: "users#signout", as: "logout_user"
 
-  get "/view_feeds", to: "feeds#view_feeds", as: "view_feeds"
-
-  get "/edit_feeds", to: "feeds#edit", as: "edit_feeds"
   
   resources :users, only: [:new, :show, :create, :update, :destroy]
-  resources :feeds, only: [:index, :create, :show,  :update, :edit, :destroy]
+  resources :feeds, only: [:index, :create, :update, :edit, :destroy]
   
 
   root "welcome#index"
