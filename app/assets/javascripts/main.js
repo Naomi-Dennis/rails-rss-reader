8a0efbb231811f96857ddd3e18010f59b4127bd0
Naomi-Dennis <njdennis94@gmail.com>
2018-08-10 17:16:13 -0400

    Change Feed View Layout

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,18 +3,20 @@
   var articleIndexButtons = document.getElementsByClassName("article-viewer")
   for (var i = 0; i < articleIndexButtons.length; i++) {
     var btn = articleIndexButtons[i]
-    btn.onclick = function(){
-      btn.style["text-decoration"] = "line-through"
-    }
+    btn.addEventListener('click', setStrikeThroughStyle)
   }
 }
 
+function setStrikeThroughStyle(event){
+event.target.parentNode.style["text-decoration"] = "line-through"
+}
 
 function init() {
   var user = document.getElementById("user")
   var body = document.getElementsByTagName("body")[0]
   var isLoggedIn = (user.dataset.checkLogin == "true")
   var image = (document.getElementById("userBackground") ? document.getElementById("userBackground").dataset.userBackground : "")
+
   if (!isLoggedIn) {
     body.style.background = "#5d687a"
   } else {
@@ -29,6 +31,7 @@ function loadDoc() {
 
   document.addEventListener("turbolinks:load", function(event){
     init();
+    modifyArticleIndexButtons()
   });
 }
 
